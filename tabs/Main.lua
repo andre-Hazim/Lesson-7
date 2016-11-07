c312ca98d26be9f9b7dc91ca78c919accf57be48
andre-Hazim at codea-scm <andre-Hazim@codea-scm.aws.mapote.com>
2016-11-07 15:41:22 +0000

    init

---
+++ tabs/Main.lua
@@ -0,0 +1,18 @@
+-- Lesson 7
+-- this program makes a button show on screen
+-- Use this function to perform your initial setup
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+    
+    -- This sets the line thickness
+    strokeWidth(5)
+   
+
+
+    -- Do your drawing here
+     sprite("Project:Blue Back Circle Button", WIDTH/2, HEIGHT/2)
+
+end
